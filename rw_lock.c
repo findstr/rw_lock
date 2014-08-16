d20e0bcd442bb098dcc7e19b86616d7170f08c98
findstr <findstr@sina.com>
2014-08-16 22:47:23 +0800

    修正了两个线程同时获得写锁时产生死锁的可能.

--- rw_lock.c
+++ rw_lock.c
@@ -5,7 +5,7 @@
 static int __lock__(struct rw_lock *lock)
 {
         while (InterlockedCompareExchange(&lock->lock, 1, 0) == 0)
-                ;
+                Sleep(0);
 
         return 0;
 }
@@ -19,6 +19,7 @@ static int __unlock__(struct rw_lock *lock)
 int rwlock_init(struct rw_lock *lock)
 {
         assert(lock);
+        lock->lock = 0;
         lock->write_ref_cnt = 0;
         lock->read_ref_cnt = 0;
 
@@ -28,20 +29,22 @@ int rwlock_init(struct rw_lock *lock)
 /* write higher */
 int rwlock_get_write(struct rw_lock *lock, int retry)
 {
+	unsigned long write_cnt;
         assert(lock);
+	__lock__(lock);
+        write_cnt = InterlockedIncrement(&lock->write_ref_cnt);
+	__unlock__(lock);
+	
+	assert(write_cnt >= 1);
 
-        __lock__(lock);
-        InterlockedIncrement(&lock->write_ref_cnt);
-        __unlock__(lock);
- 
-        while(InterlockedCompareExchange(&lock->write_ref_cnt, 1, 1) != 1)
-                ;
-
+	if (write_cnt > 1) {	/* other thread have preemptive the write lock, wait it */
+		while (lock->write_ref_cnt > 1)
+			Sleep(0);
+	}
         while (retry) {
-                if (lock->read_ref_cnt == 0) {
-                        if (lock->read_ref_cnt == 0)
-                                break;
-                }
+                if (lock->read_ref_cnt == 0)
+			break;
+
                 retry--;
                 Sleep(1);
         }
