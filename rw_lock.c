92b9066360e263f79b6a27a7cf8088adf8243cff
findstr <findstr@sina.com>
2014-08-29 15:11:30 +0800

    增加了对retry == -1的支持
    当retry == -1时， INFINITE 等�

--- rw_lock.c
+++ rw_lock.c
@@ -31,49 +31,79 @@ int rwlock_get_write(struct rw_lock *lock, int retry)
 {
 	unsigned long write_cnt;
         assert(lock);
-
-	while (retry) {
-                if (lock->write_ref_cnt <= 1) {
-                        __lock__(lock);
-                        if (lock->write_ref_cnt <= 1)
-                                break;
-                        else
-                                __unlock__(lock);
+        if (retry < 0) {
+                while (1) {
+                        if (lock->write_ref_cnt <= 1) {
+                                __lock__(lock);
+                                if (lock->write_ref_cnt <= 1)
+                                        break;
+                                else
+                                        __unlock__(lock);
+                        }
+                        Sleep(0);
                 }
-		Sleep(0);
-                retry--;
+
+                write_cnt = InterlockedIncrement(&lock->write_ref_cnt);
+                if (write_cnt <= 1) {
+                        assert(write_cnt >=  0);
+                        __unlock__(lock);
+                } else {
+                        assert(write_cnt > 1);
+                        /* other thread have preemptive the write lock, wait it */
+                        while (lock->write_ref_cnt > 1 && retry) {
+                                Sleep(0);
+                        }
+                        __unlock__(lock);
+                }
+
+                while (1) {
+                        if (lock->read_ref_cnt == 0)
+                                break;
+                        Sleep(0);
+                }
+        } else {
+                while (retry) {
+                        if (lock->write_ref_cnt <= 1) {
+                                __lock__(lock);
+                                if (lock->write_ref_cnt <= 1)
+                                        break;
+                                else
+                                        __unlock__(lock);
+                        }
+                        Sleep(1);
+                        retry--;
+                }
+
+                if (retry <= 0)
+                        return -1;
+
+                write_cnt = InterlockedIncrement(&lock->write_ref_cnt);
+                if (write_cnt <= 1) {
+                        assert(write_cnt >=  0);
+                        __unlock__(lock);
+                } else {
+                        assert(write_cnt > 1);
+                        /* other thread have preemptive the write lock, wait it */
+                        while (lock->write_ref_cnt > 1 && retry) {
+                                retry--;
+                                Sleep(1);
+                        }
+                        __unlock__(lock);
+                }
+
+                while (retry) {
+                        if (lock->read_ref_cnt == 0)
+                                break;
+
+                        retry--;
+                        Sleep(1);
+                }
+
+                if (retry <= 0) {
+                        InterlockedDecrement(&lock->write_ref_cnt);
+                        return -1;
+                }
         }
-
-        if (retry <= 0)
-                return -1;
-
-        write_cnt = InterlockedIncrement(&lock->write_ref_cnt);
-	if (write_cnt <= 1) {
-		assert(write_cnt >=  0);
-		__unlock__(lock);
-	} else {
-		assert(write_cnt > 1);
-		/* other thread have preemptive the write lock, wait it */
-		while (lock->write_ref_cnt > 1 && retry) {
-			retry--;
-			Sleep(0);
-		}
-		__unlock__(lock);
-	}
-
-        while (retry) {
-                if (lock->read_ref_cnt == 0)
-			break;
-
-                retry--;
-                Sleep(0);
-        }
-
-        if (retry <= 0) {
-                InterlockedDecrement(&lock->write_ref_cnt);
-                return -1;
-        }
-        
         return 0;
 }
 
@@ -90,25 +120,39 @@ int rwlock_put_write(struct rw_lock *lock)
 int rwlock_get_read(struct rw_lock *lock, int retry)
 {
         assert(lock);
-        
-        while (retry) {
-                if (lock->write_ref_cnt == 0) {
-                        __lock__(lock);
-                        if (lock->write_ref_cnt == 0)
-                                break;
-                        else
-                                __unlock__(lock);
+        if (retry < 0) {
+                while (1) {
+                        if (lock->write_ref_cnt == 0) {
+                                __lock__(lock);
+                                if (lock->write_ref_cnt == 0)
+                                        break;
+                                else
+                                        __unlock__(lock);
+                        }
+                        Sleep(0);
                 }
-                retry--;
-                Sleep(0);
+                InterlockedIncrement(&lock->read_ref_cnt);
+                __unlock__(lock);
+
+        } else {
+                while (retry) {
+                        if (lock->write_ref_cnt == 0) {
+                                __lock__(lock);
+                                if (lock->write_ref_cnt == 0)
+                                        break;
+                                else
+                                        __unlock__(lock);
+                        }
+                        retry--;
+                        Sleep(1);
+                }
+
+                if (retry <= 0)
+                        return -1;
+
+                InterlockedIncrement(&lock->read_ref_cnt);
+                __unlock__(lock);
         }
-
-        if (retry <= 0)
-                return -1;
-
-        InterlockedIncrement(&lock->read_ref_cnt);
-        __unlock__(lock);
-
         return 0;
 }
 
